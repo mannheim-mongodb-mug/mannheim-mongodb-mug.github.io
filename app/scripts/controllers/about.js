c3c7e8b126fec0df9ded26a958ecaaabaeb45715
Dennis Ahaus <dennis.ahaus@gmail.com>
2014-11-23 19:13:36 +0100

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mannheimMongodbMuggithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mannheimMongodbMuggithubioApp
+ */
+angular.module('mannheimMongodbMuggithubioApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mannheimMongodbMuggithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mannheimMongodbMuggithubioApp
+ */
+angular.module('mannheimMongodbMuggithubioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
